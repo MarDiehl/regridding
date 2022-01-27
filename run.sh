d84e294a0be0f001d92ff9473061874c61132882
Martin Diehl <mail@martin-diehl.net>
2022-01-27 10:09:17 +0100

    polishing

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DAMASK_grid -l tensionX.yaml -g onephase.vti
+DAMASK_grid -l ../tensionX.yaml -g simple.vti
 #DAMASK_grid -l tensionX.yaml -g simple.vti -r 120 # normal restart (should work)
-./regridding.py
-DAMASK_grid -l tensionX.yaml -g onephase-rg.vti -r 120 # normal restart (should work)
+../regridding.py
+DAMASK_grid -l ../tensionX.yaml -g simple-rg.vti -r 120 # normal restart (should work)
