46b34cbf6b93410b2c384874c22b2a205b4131c5
Martin Diehl <mail@martin-diehl.net>
2022-01-27 10:45:28 +0100

    faster

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 DAMASK_grid -l ../tensionX.yaml -g simple.vti
-#DAMASK_grid -l tensionX.yaml -g simple.vti -r 120 # normal restart (should work)
+#DAMASK_grid -l tensionX.yaml -g simple.vti -r 14 # normal restart (should work)
 ../regridding.py
-DAMASK_grid -l ../tensionX.yaml -g simple-rg.vti -r 120 # normal restart (should work)
+DAMASK_grid -l ../tensionX.yaml -g simple-rg.vti -r 14 # normal restart (should work)
